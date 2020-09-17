f44793461c7ce246f90f80c0526cd15aad395e0f
Philipp Rudiger <prudiger@anaconda.com>
2020-09-17 15:22:15 +0200

    Added initial tests

---
+++ monitor/tests/test_schema.py
@@ -0,0 +1,20 @@
+import panel as pn
+
+from monitor.schema import JSONSchema
+
+def test_boolean_schema():
+    json_schema = JSONSchema(schema={'bool': {'type': 'boolean'}})
+    assert isinstance(json_schema._widgets['bool'], pn.widgets.Checkbox)
+
+def test_number_schema():
+    json_schema = JSONSchema(schema={'number': {'type': 'number', 'minimum': 0, 'maximum': 3.14}})
+    widget = json_schema._widgets['number']
+    assert isinstance(widget, pn.widgets.FloatSlider)
+    assert widget.start == 0
+    assert widget.end == 3.14
+    
+def test_enum_schema():
+    json_schema = JSONSchema(schema={'enum': {'type': 'enum', 'enum': ['A', 'B', 'C']}})
+    widget = json_schema._widgets['enum']
+    assert isinstance(widget, pn.widgets.Select)
+    assert widget.options == ['A', 'B', 'C']
