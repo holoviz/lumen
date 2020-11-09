a49dbbd6511c4ce505c6f35ad05592151bb82719
Philipp Rudiger <prudiger@anaconda.com>
2020-11-09 16:54:05 +0100

    Updated tests

--- lumen/tests/test_schema.py
+++ lumen/tests/test_schema.py
@@ -4,18 +4,22 @@ from lumen.schema import JSONSchema
 
 
 def test_boolean_schema():
-    json_schema = JSONSchema(schema={'bool': {'type': 'boolean'}})
+    json_schema = JSONSchema(schema={'bool': {'type': 'boolean'}}, multi=False)
     assert isinstance(json_schema._widgets['bool'], pn.widgets.Checkbox)
 
 def test_number_schema():
-    json_schema = JSONSchema(schema={'number': {'type': 'number', 'minimum': 0, 'maximum': 3.14}})
+    json_schema = JSONSchema(multi=False, schema={
+        'number': {'type': 'number', 'inclusiveMinimum': 0, 'inclusiveMaximum': 3.14}
+    })
     widget = json_schema._widgets['number']
     assert isinstance(widget, pn.widgets.FloatSlider)
     assert widget.start == 0
     assert widget.end == 3.14
     
 def test_enum_schema():
-    json_schema = JSONSchema(schema={'enum': {'type': 'enum', 'enum': ['A', 'B', 'C']}})
+    json_schema = JSONSchema(multi=False, schema={
+        'enum': {'type': 'enum', 'enum': ['A', 'B', 'C']}
+    })
     widget = json_schema._widgets['enum']
     assert isinstance(widget, pn.widgets.Select)
     assert widget.options == ['A', 'B', 'C']
