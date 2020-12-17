4758d2ef772c7b9335701f2a2bfcd3db26a70828
Philipp Rudiger <prudiger@anaconda.com>
2020-12-17 19:50:35 +0100

    Improve Intake Source

--- lumen/sources/intake.py
+++ lumen/sources/intake.py
@@ -10,38 +10,58 @@ class IntakeSource(Source):
     An IntakeSource loads data from an Intake catalog.
     """
 
-    catalog = param.String(doc="""
-        The name of the catalog; must be provided.""")
-
     dask = param.Boolean(default=False, doc="""
         Whether to return a dask DataFrame.""")
 
-    filename = param.String(doc="Filename of the catalog file.")
+    filename = param.String(doc="Deprecated alias for the URI")
+
+    uri = param.String(doc="URI of the catalog file.")
+
+    specification = param.Dict(default="An inlined Catalog specification.")
 
     source_type = 'intake'
 
     def __init__(self, **params):
         super().__init__(**params)
-        self.cat = intake.open_catalog(self.filename)
-        if not self.catalog:
-            cats = list(self.cat)
-            if len(cats) == 1:
-                self.catalog = cats[0]
-            else:
-                raise ValueError("Could not determine catalog to load.")
-        elif self.catalog not in self.cat:
-            raise ValueError(f"'{self.catalog}' not found in catalog.'")
+        if self.filename:
+            self.param.warning("IntakeSource filename parameter is "
+                               "deprecated in favor of uri parameter.")
+            self.uri = self.filename
+        if self.uri and self.specification:
+            raise ValueError("Either specify a Catalog uri or an "
+                             "inlined catalog specification, not both.")
+        elif self.uri:
+            self.cat = intake.open_catalog(self.uri)
+        elif self.specification:
+            result = intake.catalog.local.CatalogParser(self.specification)
+            if result.errors:
+                raise intake.catalog.exceptions.ValidationError(
+                    "Catalog '{}' has validation errors:\n\n{}"
+                    "".format(self.path, "\n".join(result.errors)), result.errors)
+            cfg = result.data
+            self.cat = Catalog(cfg.pop('data_sources'), **cfg)
+
+    def _read(self, table, dask=True):
+        if self.dask or dask:
+            try:
+                return self.cat[table].to_dask()
+            except Exception:
+                if self.dask:
+                    self.param.warning(f"Could not load {table} table with dask.")
+                pass
+        return self.cat[table].read()
 
     def get_schema(self, table=None):
         if table:
-            cat = self.cat[table].to_dask()
+            cat = self.get(table, dask=True)
             return get_dataframe_schema(cat)['items']['properties']
         else:
-            return {name: get_dataframe_schema(self.cat[name].to_dask())['items']['properties']
+            return {name: get_dataframe_schema(self.get(name, dask=True))['items']['properties']
                     for name in list(self.cat)}
 
     @cached()
     def get(self, table, **query):
-        df = self.cat[table].to_dask()
+        dask = query.pop('dask', self.dask)
+        df = self._read(table)
         df = self._filter_dataframe(df, **query)
-        return df if self.dask else df.compute()
+        return df if dask or not hasattr(df, 'compute') else df.compute()
