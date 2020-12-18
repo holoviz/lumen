b4764801b719c55ef37aaef73001aaef14fd29b6
Philipp Rudiger <prudiger@anaconda.com>
2020-12-18 16:10:54 +0100

    Add MANIFEST.in

--- lumen/tests/test_dashboard.py
+++ lumen/tests/test_dashboard.py
@@ -5,7 +5,7 @@ from lumen.views import View
 
 def test_dashboard_with_local_view():
     root = os.path.dirname(__file__)
-    dashboard = Dashboard(os.path.join(root, 'sample_dashboard', 'dashboard.yaml'))
+    dashboard = Dashboard(os.path.join(root, 'sample_dashboard', 'dashboard.yml'))
     target = dashboard._targets[0]
     view = View.from_spec(target.views[0], target, target.source, [])
     assert isinstance(view, dashboard._modules['views'].TestView)
