6903261d96950a9cabc6abf412d6c6231a739f5c
Philipp Rudiger <prudiger@anaconda.com>
2021-01-05 13:10:31 +0100

    Refactored AE5 source

--- lumen/sources/ae5.py
+++ lumen/sources/ae5.py
@@ -12,6 +12,11 @@ from ..util import get_dataframe_schema
 
 
 class AE5Source(Source):
+    """
+    The AE5Source provides a number of tables, which allow monitoring
+    the nodes, deployments, sessions and resource profiles of an
+    Anaconda Enterprise 5 installation.
+    """
 
     hostname = param.String(doc="URL of the AE5 host.")
 
@@ -31,12 +36,15 @@ class AE5Source(Source):
     source_type = 'ae5'
 
     _deployment_columns = [
-        'id', 'name', 'url', 'owner', 'resource_profile', 'public', 'state'
+        'id', 'name', 'url', 'owner', 'resource_profile', 'public', 'state',
+        'cpu', 'cpu_percent', 'memory', 'memory_percent', 'uptime', 'restarts'
     ]
 
     _session_columns = [
         'id', 'name', 'url', 'owner', 'resource_profile', 'state'
     ]
+
+    _tables = ['deployments', 'nodes', 'resources', 'sessions']
 
     _units = {
         'm': 0.001,
@@ -68,18 +76,19 @@ class AE5Source(Source):
         scale_factor = cls._units[val_unit]
         return float(value[:-len(val_unit)]) * scale_factor
 
-    def _get_record(self, deployment):
-        container_info = deployment.get('_k8s', {}).get('containers', {})
-        record = {
-            "id": deployment['id'],
-            "name": deployment['name'],
-            "url": deployment['url'],
-            "resource_profile": deployment['resource_profile']
-        }
+    @property
+    def _user(self):
+        return state.headers.get('Anaconda-User') if self.private else None
 
+    def _process_deployment(self, deployment):
+        container_info = deployment.get('_k8s', {}).get('containers', {})
         if 'app' not in container_info:
             nan = float('NaN')
-            return dict(record, cpu=nan, memory=nan, restarts=nan, uptime='-')
+            deployment['cpu'] = nan
+            deployment['memory'] = nan
+            deployment['restarts'] = nan
+            deployment['uptime'] = '-'
+            return deployment
 
         container = container_info['app']
         limits = container['limits']
@@ -89,93 +98,88 @@ class AE5Source(Source):
         cpu_cap = self._convert_value(limits['cpu'])
         if 'cpu' in usage:
             cpu = self._convert_value(usage['cpu'])
-            record['cpu'] = round((cpu / cpu_cap)*100, 2)
+            deployment['cpu'] = cpu
+            deployment['cpu_percent'] = round((cpu / cpu_cap)*100, 2)
         else:
-            record['cpu'] = float('nan')
+            deployment['cpu_percent'] = float('nan')
+            deployment['cpu'] = float('nan')
 
         # Memory usage
         mem_cap = self._convert_value(limits['memory'])
         if 'memory' in usage:
             mem = self._convert_value(usage['memory'])
-            record['memory'] = round((mem / mem_cap)*100, 2)
+            deployment['memory'] = mem
+            deployment['memory_percent'] = round((mem / mem_cap)*100, 2)
         else:
-            record['memory'] = float('nan')
+            deployment['memory'] = float('nan')
+            deployment['memory_percent'] = float('nan')
 
         # Uptime
         started = container.get('since')
-        record["restarts"] = container['restarts']
+        deployment["restarts"] = container['restarts']
         if started:
             started_dt = dt.datetime.fromisoformat(started.replace('Z', ''))
             uptime = str(dt.datetime.now()-started_dt)
-            record["uptime"] = uptime[:uptime.index('.')]
+            deployment["uptime"] = uptime[:uptime.index('.')]
         else:
-            record["uptime"] = '-'
+            deployment["uptime"] = '-'
 
-        return record
+        return deployment
 
-    @property
-    def _user(self):
-        return state.headers.get('Anaconda-User') if self.private else None
+    def _process_nodes(self, node):
+        caps = {
+            'cpu': self._convert_value(node['capacity/cpu']),
+            'gpu': self._convert_value(node['capacity/gpu']),
+            'mem': self._convert_value(node['capacity/mem']),
+            'pod': int(node['capacity/pod'])
+        }
+        for column in node.index:
+            if 'capacity' in column or '/' not in column:
+                continue
+            vtype = column.split('/')[1]
+            cap = caps[vtype]
+            value = node[column]
+            if vtype != 'pod':
+                value = self._convert_value(value)
+            node[column] = value
+            node[f'{column}_percent'] = 0 if cap == 0 else round((value/caps[vtype])*100, 2)
+        return node
 
-    def _filter_deployments(self, deployments):
+    def _get_deployments(self):
         user = self._user
-        if not self.private or user is None:
-            return deployments
+        deployments = self._session.deployment_list(
+            k8s=True, format='dataframe', collaborators=bool(user)
+        ).apply(self._process_deployment, axis=1)
+        if user is None:
+            return deployments[self._deployment_columns]
         return deployments[
             deployments.public |
             (deployments.owner == user) |
             deployments._collaborators.apply(lambda x: user in x)
-        ]
+        ][self._deployment_columns]
 
-    def get_schema(self, table=None):
-        schemas = {}
-        if table in (None, 'deployments'):
-            deployments = self.get('deployments')
-            schemas['deployments'] = get_dataframe_schema(deployments)['items']['properties']
-        if table in (None, 'sessions'):
-            sessions = self.get('sessions')
-            schemas['sessions'] = get_dataframe_schema(sessions)['items']['properties']
-        if table in (None, 'usage'):
-            usage = self.get('usage')
-            schemas['usage'] = get_dataframe_schema(usage)['items']['properties']
-        if table in (None, 'resources'):
-            resources = self.get('resources')
-            schemas['resources'] = get_dataframe_schema(resources)['items']['properties']
-        return schemas if table is None else schemas[table]
+    def _get_nodes(self):
+        nodes = self._session.node_list(format='dataframe').apply(self._process_nodes, axis=1)
+        return nodes[[c for c in nodes.columns if not c.startswith('_')]]
+
+    def _get_resources(self):
+        return self._session.resource_profile_list(format='dataframe')
+
+    def _get_sessions(self):
+        sessions = self._session.session_list(format='dataframe')
+        if self._user:
+            sessions = sessions[sessions.owner==self._user]
+        return sessions[self._session_columns]
 
     @cached(with_query=False)
     def get(self, table, **query):
-        if table not in ('deployments', 'resources', 'sessions', 'usage'):
-            raise ValueError(f"AE5Source has no '{table}' table, choose "
-                             "from 'deployments', 'resources', 'usage', "
-                             "'sessions'.")
-        if table == 'deployments':
-            deployments = self._session.deployment_list(
-                format='dataframe', collaborators=bool(self._user)
-            )
-            deployments = self._filter_deployments(deployments)
-            return deployments[self._deployment_columns]
-        elif table == 'resources':
-            resources = self._session.resource_profile_list(format='dataframe')
-            return resources
-        elif table == 'usage':
-            records = []
-            user = self._user
-            for deployment in self._session.deployment_list(k8s=True, collaborators=bool(user)):
-                public = deployment['public']
-                owner = deployment['owner']
-                collaborators = deployment.get('_collaborators')
-                if user and (not public and user != owner and user not in collaborators):
-                    continue
-                record = self._get_record(deployment)
-                if record is None:
-                    continue
-                records.append(record)
-            columns = ['id', 'name', 'url', 'resource_profile', 'cpu',
-                       'memory', 'uptime', 'restarts']
-            return pd.DataFrame(records, columns=columns)
-        else:
-            sessions = self._session.session_list(format='dataframe')
-            if self._user:
-                sessions = sessions[sessions.owner==self._user]
-            return sessions[self._session_columns]
+        if table not in self._tables:
+            raise ValueError(f"AE5Source has no '{table}' table, choose from {repr(self._tables)}.")
+        return getattr(self, f'_get_{table}')()
+
+    def get_schema(self, table=None):
+        schemas = {}
+        for t in self._tables:
+            if table is None or t == table:
+                schemas[t] = get_dataframe_schema(self.get(t))['items']['properties']
+        return schemas if table is None else schemas[table]
