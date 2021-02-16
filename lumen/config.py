2590d245195108784f8ffc99114b92f8e2bafe45
Philipp Rudiger <prudiger@anaconda.com>
2021-02-16 18:53:49 +0100

    Refactor of Dashboard and Target classes (#78)

--- lumen/config.py
+++ lumen/config.py
@@ -1,5 +1,8 @@
 import param as param
 
+from .filters import Filter
+from .sources import Source
+
 
 class _config(param.Parameterized):
     """
@@ -18,6 +21,26 @@ class _config(param.Parameterized):
 
     yamls = param.List(default=[], doc="""
       List of yaml files currently being served.""")
+
+    def load_global_sources(self, sources, root, clear_cache=True):
+        """
+        Loads global sources shared across all targets.
+        """
+        for name, source_spec in sources.items():
+            if source_spec.get('shared'):
+                source_spec = dict(source_spec)
+                filter_specs = source_spec.pop('filters', {})
+                self.sources[name] = source = Source.from_spec(
+                    source_spec, self.sources, root=root
+                )
+                if source.cache_dir and clear_cache:
+                    source.clear_cache()
+                schema = source.get_schema()
+                self.filters[name] = {
+                    fname: Filter.from_spec(filter_spec, schema)
+                    for fname, filter_spec in filter_specs.items()
+                }
+
 
 
 config = _config()
