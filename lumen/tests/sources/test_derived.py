3762a9f44ef7577c931611fdef2bbf339e5e457e
Philipp Rudiger <prudiger@anaconda.com>
2021-04-07 13:24:48 +0200

    Add derived source filter tests

--- lumen/tests/sources/test_derived.py
+++ lumen/tests/sources/test_derived.py
@@ -44,6 +44,31 @@ def test_derived_mirror_source_transforms():
     }
 
 
+def test_derived_mirror_source_filters():
+    root = os.path.dirname(__file__)
+    original = FileSource(tables={'test': 'test.csv'}, root=root, kwargs={'parse_dates': ['D']})
+    derived = DerivedSource.from_spec({
+        'type': 'derived',
+        'source': 'original',
+        'filters': [{'type': 'constant', 'field': 'A', 'value': (0, 2)}]
+    }, sources={'original': original})
+
+    assert derived.get_tables() == ['test']
+    df = pd._testing.makeMixedDataFrame().iloc[:3]
+    pd.testing.assert_frame_equal(derived.get('test'), df)
+    assert derived.get_schema('test') == {
+        'A': {'inclusiveMaximum': 2.0, 'inclusiveMinimum': 0.0, 'type': 'number'},
+        'B': {'inclusiveMaximum': 1.0, 'inclusiveMinimum': 0.0, 'type': 'number'},
+        'C': {'enum': ['foo1', 'foo2', 'foo3'], 'type': 'string'},
+        'D': {
+            'format': 'datetime',
+            'type': 'string',
+            'inclusiveMaximum': '2009-01-05T00:00:00',
+            'inclusiveMinimum': '2009-01-01T00:00:00'
+        }
+    }
+
+
 def test_derived_tables_source():
     root = os.path.dirname(__file__)
     original = FileSource(tables={'test': 'test.csv'}, root=root, kwargs={'parse_dates': ['D']})
@@ -68,6 +93,36 @@ def test_derived_tables_source_transforms():
                 'source': 'original',
                 'table': 'test',
                 'transforms': [{'type': 'iloc', 'end': 3}]
+            }
+        }
+    }, sources={'original': original})
+
+    assert derived.get_tables() == ['derived']
+    df = pd._testing.makeMixedDataFrame().iloc[:3]
+    pd.testing.assert_frame_equal(derived.get('derived'), df)
+    assert derived.get_schema('derived') == {
+        'A': {'inclusiveMaximum': 2.0, 'inclusiveMinimum': 0.0, 'type': 'number'},
+        'B': {'inclusiveMaximum': 1.0, 'inclusiveMinimum': 0.0, 'type': 'number'},
+        'C': {'enum': ['foo1', 'foo2', 'foo3'], 'type': 'string'},
+        'D': {
+            'format': 'datetime',
+            'type': 'string',
+            'inclusiveMaximum': '2009-01-05T00:00:00',
+            'inclusiveMinimum': '2009-01-01T00:00:00'
+        }
+    }
+
+
+def test_derived_tables_source_transforms():
+    root = os.path.dirname(__file__)
+    original = FileSource(tables={'test': 'test.csv'}, root=root, kwargs={'parse_dates': ['D']})
+    derived = DerivedSource.from_spec({
+        'type': 'derived',
+        'tables': {
+            'derived': {
+                'source': 'original',
+                'table': 'test',
+                'filters': [{'type': 'constant', 'field': 'A', 'value': (0, 2)}]
             }
         }
     }, sources={'original': original})
