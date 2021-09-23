a8c6098de4c0e6926eb2103bea00b08fad816bb1
Philipp Rudiger <prudiger@anaconda.com>
2021-09-23 14:45:37 +0200

    Link WidgetFilter to widget bi-directionally (#182)
    * Link WidgetFilter bi-directionally
    
    * Add test
    
    * Drop py3.6 from test suite
    
    * Fix tox

--- lumen/tests/filters/test_base.py
+++ lumen/tests/filters/test_base.py
@@ -3,3 +3,27 @@ from lumen.filters import Filter
 
 def test_resolve_module_type():
     assert Filter._get_type('lumen.filters.base.Filter') is Filter
+
+
+def test_widget_filter_link():
+    wfilter = Filter.from_spec(
+        {'type': 'widget', 'field': 'test'},
+        {'example': {
+            'test': {
+                'type': 'integer',
+                'inclusiveMinimum': 0,
+                'inclusiveMaximum': 2
+            }
+        }}
+    )
+    widget = wfilter.panel
+
+    assert widget.value == (0, 2)
+    
+    widget.value = (1, 2)
+
+    assert wfilter.value == (1, 2)
+
+    wfilter.value = (2, 2)
+
+    assert widget.value == (2, 2)
