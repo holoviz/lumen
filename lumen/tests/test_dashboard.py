31cb6eb76a944148c04007140611f6d6ac7254ae
Philipp Rudiger <prudiger@anaconda.com>
2021-11-24 13:25:05 +0100

    Add filter query_sync test

--- lumen/tests/test_dashboard.py
+++ lumen/tests/test_dashboard.py
@@ -23,3 +23,21 @@ def test_dashboard_with_url_sync(set_root, document):
     assert pn.state.location.search == '?target=0'
     pn.state.location.search = '?target=1'
     assert dashboard._layout.active == 1
+
+
+def test_dashboard_with_url_sync_filters(set_root, document):
+    root = pathlib.Path(__file__).parent / 'sample_dashboard'
+    set_root(str(root))
+    dashboard = Dashboard(str(root / 'sync_query_filters.yml'))
+    dashboard._render_dashboard()
+    target = dashboard.targets[0]
+    f1, f2 = target.filters
+    f1.value = (0.1, 0.7)
+    assert pn.state.location.search == '?A=%5B0.1%2C+0.7%5D'
+    pn.state.location.search = '?A=%5B0.3%2C+0.8%5D'
+    assert f1.value == [0.3, 0.8]
+    f2.value = ['foo1', 'foo2']
+    assert pn.state.location.search == '?A=%5B0.3%2C+0.8%5D&C=%5B%22foo1%22%2C+%22foo2%22%5D'
+    pn.state.location.search = '?A=%5B0.3%2C+0.8%5D&C=%5B%22foo1%22%2C+%22foo2%22%2C+%22foo3%22%5D'
+    assert f2.value == ['foo1', 'foo2', 'foo3']
+    
