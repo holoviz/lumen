2b4a833c9fa35de25ae0b0c8ddec6456499dff08
Eli Pinkus <eli.pinkus@blackstone.com>
2022-01-03 21:45:10 +0000

    Implement base and example sql transform

---
+++ lumen/transforms/sql.py
@@ -0,0 +1,54 @@
+from jinja2 import Template
+from .base import Transform
+import param
+
+class SQLTransform(Transform):
+        
+    __abstract = True
+    
+    def apply(self, sql_in):
+        """
+        Given an SQL statement, manipulate it, and return a new SQL statement.
+        
+        Parameters
+        ----------
+        sql_in: string
+            The initial SQL query to be manipulated.
+            
+        Returns
+        -------
+        string
+            New SQL query derived from the above query.
+        """
+        return sql_in
+    
+
+class SQLGroupBy(Transform):
+    """
+    Performs a Group-By and aggregation
+    """
+    
+    by = param.List(doc="""
+        Columns to Group by""")
+    
+    aggregates = param.Dict(doc="""
+        mapping of Aggregate Functions to use to which column to use them on""")
+    
+    transform_type = 'sql_group_by'
+    
+    def apply(self, sql_in):
+        
+        
+        template = """
+            SELECT
+                {{by_cols}},
+                {{aggs}}
+            FROM ( {{sql_in}} )
+            GROUP BY {{by_cols}}
+        """
+        
+        return Template(template, trim_blocks=True, lstrip_blocks=True).render(
+            by_cols=', '.join(self.by),
+            aggs=', '.join([f'{agg}({col})' for agg, col in self.aggregates.items()]),
+            sql_in=sql_in
+        )
