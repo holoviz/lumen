6bcd85106b907ecf63f4979a1ac930046919a1af
Eli Pinkus <eli.pinkus@blackstone.com>
2022-01-04 00:25:30 +0000

    almost there

--- lumen/sources/intake_sql.py
+++ lumen/sources/intake_sql.py
@@ -7,9 +7,9 @@ class IntakeSQLSource(IntakeSource):
     
     @cached(with_query=True)
     def get(self, table, **query):
-        import pdb; pdb.set_trace()
         dask = query.pop('__dask', self.dask)
         sql_transforms = query.pop('sql_transforms', [])
+        # TODO this obj is not subscriptable
         for sql_transform in sql_transforms:
             self.cat[table]['sql_expr'] = sql_transform.apply(self.cat[table]['sql_expr'])
         df = self._read(table)
