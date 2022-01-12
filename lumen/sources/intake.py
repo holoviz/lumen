9c2013b4ccf910bec430d8c859b9b3eb65b36942
Eli Pinkus <eli.pinkus@blackstone.com>
2022-01-12 15:51:04 +0000

    removing commented lines

--- lumen/sources/intake.py
+++ lumen/sources/intake.py
@@ -109,17 +109,3 @@ class IntakeDremioSource(IntakeBaseSource):
             self.uri, cert=self.cert, tls=self.tls, username=self.username,
             password=self.password
         )
-
-# class IntakeSQLSource(IntakeBaseSource):
-    
-#     source_type = 'intake_sql'
-    
-#     @cached(with_query=True)
-#     def get(self, table, **query):
-#         import pdb; pdb.set_trace()
-#         dask = query.pop('__dask', self.dask)
-#         sql_transforms = query.pop('sql_transforms', [])
-#         for sql_transform in sql_transforms:
-#             self.cat[table]['sql_expr'] = sql_transform.apply(self.cat[table]['sql_expr'])
-#         df = self._read(table)
-#         return df if dask or not hasattr(df, 'compute') else df.compute()
