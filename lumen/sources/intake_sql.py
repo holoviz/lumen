5490683ad325c163bdad55613cb576f8e54123b4
Eli Pinkus <eli.pinkus@blackstone.com>
2022-01-12 21:11:50 +0000

    create temp new catalog for data pull instead of altering sources catalog

--- lumen/sources/intake_sql.py
+++ lumen/sources/intake_sql.py
@@ -11,42 +11,58 @@ class IntakeSQLSource(IntakeSource):
     """
     source_type = 'intake_sql'
     
-    def _update_sql(self, table, new_sql_expr):
+    def _read(self, table, cat, dask=True):
+        try:
+            entry = cat[table]
+        except KeyError:
+            raise KeyError(f"'{table}' table could not be found in Intake "
+                           "catalog. Available tables include: "
+                           f"{list(self.cat)}.")
+        if self.dask or dask:
+            try:
+                return entry.to_dask()
+            except Exception:
+                if self.dask:
+                    self.param.warning(f"Could not load {table} table with dask.")
+                pass
+        return entry.read()
+    
+    def _update_sql(self, table, new_sql_expr, cat):
         """
         Updates a table's sql statement.
         """
 
-        self.cat = self.cat.add(
+        return cat.add(
             SQLSource(
-                uri=self.cat[table]._uri,
+                uri=cat[table]._uri,
                 sql_expr=new_sql_expr,
-                sql_kwargs=self.cat[table]._sql_kwargs,
-                metadata=self.cat[table].metadata,
+                sql_kwargs=cat[table]._sql_kwargs,
+                metadata=cat[table].metadata,
             ),
             table,
             path='temp/temp.yaml'
         )
 
     
-    def _apply_sql_transform(self, table, transform):
+    def _apply_sql_transform(self, table, transform, cat):
         """
         Applies a transformation and subsequently updates a table's
         sql statement.
         """
-        sql_in = self.cat[table]._sql_expr
+        sql_in = cat[table]._sql_expr
         sql_out = transform.apply(sql_in)
-        self._update_sql(table, sql_out)
+        return self._update_sql(table, sql_out, cat)
     
     # TODO: enable caching by hash of final sql_statement
     def get(self, table, **query):
+        import pdb; pdb.set_trace()
         dask = query.pop('__dask', self.dask)
         sql_transforms = query.pop('sql_transforms', [])
         
+        new_cat = self.cat
         for sql_transform in sql_transforms:
-            self._apply_sql_transform(table, sql_transform)
+            new_cat = self._apply_sql_transform(table, sql_transform, new_cat)
 
-        df = self._read(table)
+        df = self._read(table, new_cat)
         
-        # Hack to reset the catalog obj. after the data pull
-        self.cat = intake.open_catalog(self.uri)
         return df if dask or not hasattr(df, 'compute') else df.compute()
