65c5e14b3a3501d6e9e1b76f84261916faea6ec1
Philipp Rudiger <prudiger@anaconda.com>
2022-02-10 14:56:47 +0100

    Fix URLParam callback

--- lumen/variables/base.py
+++ lumen/variables/base.py
@@ -138,7 +138,7 @@ class URLQuery(Variable):
             pn.state.location.param.watch(self._update_value, 'search')
             self._update_value()
 
-    def _update_value(self):
+    def _update_value(self, event):
         self.value = pn.state.location.query_params.get(self.key, self.default)
 
 
