ba9ddf1ca0db29ad0881ab8b97755b04f0e2c7c5
Philipp Rudiger <philipp.jfr@gmail.com>
2022-05-10 16:16:36 +0200

    Add support NULL queries in SQLFilter

--- lumen/tests/sources/test_intake_sql.py
+++ lumen/tests/sources/test_intake_sql.py
@@ -44,6 +44,32 @@ def test_intake_sql_get_schema():
     assert 'test' in source._schema_cache
     assert 'test_sql' in source._schema_cache
 
+def test_intake_sql_get_schema_with_none():
+    root = os.path.dirname(__file__)
+    source = IntakeSQLSource(
+        uri=os.path.join(root, 'catalog.yml'), root=root
+    )
+    expected_sql = {
+        'A': {'inclusiveMaximum': 4.0, 'inclusiveMinimum': 0.0, 'type': 'number'},
+        'B': {'inclusiveMaximum': 1.0, 'inclusiveMinimum': 0.0, 'type': 'number'},
+        'C': {'enum': ['foo1', None, 'foo3', 'foo5'], 'type': 'string'},
+        'D': {
+            'format': 'datetime',
+            'inclusiveMaximum': '2009-01-07 00:00:00',
+            'inclusiveMinimum': '2009-01-01 00:00:00',
+            'type': 'string'
+        }
+    }
+    expected_csv = dict(expected_sql, D={
+        'format': 'datetime',
+        'inclusiveMaximum': '2009-01-07T00:00:00',
+        'inclusiveMinimum': '2009-01-01T00:00:00',
+        'type': 'string'
+    })
+    assert source.get_schema('test_sql_with_none') == expected_sql
+    assert 'test' not in source._schema_cache
+    assert 'test_sql_with_none' in source._schema_cache
+
 def test_intake_sql_transforms():
     root = os.path.dirname(__file__)
     source = IntakeSQLSource(
@@ -64,6 +90,18 @@ def test_intake_sql_filter_int():
     df = pd._testing.makeMixedDataFrame()
     filtered = source.get('test_sql', A=1)
     expected = df[df.A==1].reset_index(drop=True)
+    pd.testing.assert_frame_equal(filtered, expected)
+    source.clear_cache()
+
+def test_intake_sql_filter_None():
+    root = os.path.dirname(__file__)
+    source = IntakeSQLSource(
+        uri=os.path.join(root, 'catalog.yml'), root=root
+    )
+    df = pd._testing.makeMixedDataFrame()
+    filtered = source.get('test_sql_with_none', C=None)
+    expected = df[(df.A==1) | (df.A==3)].reset_index(drop=True)
+    expected['C'] = None
     pd.testing.assert_frame_equal(filtered, expected)
     source.clear_cache()
 
@@ -146,6 +184,18 @@ def test_intake_sql_filter_list():
     filtered = source.get('test_sql', C=['foo1', 'foo3'])
     expected = df[df.C.isin(['foo1', 'foo3'])].reset_index(drop=True)
     pd.testing.assert_frame_equal(filtered, expected)
+
+def test_intake_sql_filter_list_with_None():
+    root = os.path.dirname(__file__)
+    source = IntakeSQLSource(
+        uri=os.path.join(root, 'catalog.yml'), root=root
+    )
+    df = pd._testing.makeMixedDataFrame()
+    filtered = source.get('test_sql_with_none', C=[None, 'foo5'])
+    expected = df[df.A.isin([1, 3, 4])].reset_index(drop=True)
+    expected['C'] = [None, None, 'foo5']
+    pd.testing.assert_frame_equal(filtered, expected)
+    source.clear_cache()
 
 def test_intake_sql_transforms_cache():
     root = os.path.dirname(__file__)
