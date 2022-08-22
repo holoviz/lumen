7b93db674174bb59808f507951ec0e7c7a45f74c
thuydotm <thuydtm.csk9@gmail.com>
2022-08-22 21:09:19 +0700

    Refator Source tests (#302)

--- lumen/tests/sources/test_base.py
+++ lumen/tests/sources/test_base.py
@@ -4,82 +4,124 @@ import os
 from pathlib import Path
 
 import pandas as pd
+import pytest
 
 from lumen.sources import Source
 from lumen.state import state
 from lumen.transforms.sql import SQLLimit
 
 
-def test_resolve_module_type():
+@pytest.fixture
+def source(make_filesource):
+    root = os.path.dirname(__file__)
+    return make_filesource(root)
+
+
+@pytest.fixture
+def expected_df(column_value_type):
+    df = pd._testing.makeMixedDataFrame()
+    column, value, type = column_value_type
+
+    if type == 'single_value':
+        return df[df[column] == value]
+
+    elif type == 'range':
+        begin, end = value
+        return df[(df[column] >= begin) & (df[column] <= end)]
+
+    elif type == 'range_list':
+        conditions = False
+        for range in value:
+            begin, end = range
+            conditions |= ((df[column] >= begin) & (df[column] <= end))
+        return df[conditions]
+
+    elif type == 'list':
+        return df[df[column].isin(value)]
+
+    elif type == 'date':
+        return df[df[column] == pd.to_datetime(value)]
+
+    elif type == 'date_range':
+        begin, end = value
+        return df[(df[column] >= pd.to_datetime(begin)) & (df[column] <= pd.to_datetime(end))]
+
+    return df
+
+
+def test_source_resolve_module_type():
     assert Source._get_type('lumen.sources.base.Source') is Source
 
-def test_source_table_cache_key(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    assert source._get_key('test') == source._get_key('test')
 
-def test_source_table_and_int_query_cache_key(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    assert source._get_key('test', A=314) == source._get_key('test', A=314)
+@pytest.mark.parametrize("filter_col_A", [314, (13, 314), ['A', 314, 'def']])
+@pytest.mark.parametrize("filter_col_B", [(3, 15.9)])
+@pytest.mark.parametrize("filter_col_C", [[1, 'A', 'def']])
+@pytest.mark.parametrize("sql_transforms", [(None, None), (SQLLimit(limit=100), SQLLimit(limit=100))])
+def test_source_table_cache_key(source, filter_col_A, filter_col_B, filter_col_C, sql_transforms):
+    t1, t2 = sql_transforms
+    kwargs1 = {}
+    kwargs2 = {}
+    if t1 is not None:
+        kwargs1['sql_transforms'] = [t1]
+    if t2 is not None:
+        kwargs2['sql_transforms'] = [t2]
+    key1 = source._get_key('test', A=filter_col_A, B=filter_col_B, C=filter_col_C, **kwargs1)
+    key2 = source._get_key('test', A=filter_col_A, B=filter_col_B, C=filter_col_C, **kwargs2)
+    assert key1 == key2
 
-def test_source_table_and_range_query_cache_key(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    assert source._get_key('test', A=(13, 314)) == source._get_key('test', A=(13, 314))
 
-def test_source_table_and_list_query_cache_key(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    assert source._get_key('test', A=['A', 314, 'def']) == source._get_key('test', A=['A', 314, 'def'])
+@pytest.mark.parametrize(
+    "column_value_type", [
+        ('A', 1, 'single_value'),
+        ('A', (1, 3), 'range'),
+        ('A', (1, 2), 'range'),
+        ('A', [(0, 1), (3, 4)], 'range_list'),
+        ('C', 'foo2', 'single_value'),
+        ('C', ['foo1', 'foo3'], 'list'),
+        ('D', dt.datetime(2009, 1, 2), 'single_value'),
+        ('D', (dt.datetime(2009, 1, 2), dt.datetime(2009, 1, 5)), 'range'),
+        ('D', [dt.datetime(2009, 1, 2), dt.datetime(2009, 1, 5)], 'list'),
+        ('D', dt.datetime(2009, 1, 2), 'date'),
+        ('D', (dt.date(2009, 1, 2), dt.date(2009, 1, 5)), 'date_range'),
+    ]
+)
+@pytest.mark.parametrize("dask", [True, False])
+def test_file_source_filter(source, column_value_type, dask, expected_df):
+    column, value, _ = column_value_type
+    kwargs = {column: value, '__dask': dask}
+    filtered = source.get('test', **kwargs)
+    pd.testing.assert_frame_equal(filtered, expected_df)
 
-def test_source_table_and_sql_transform_cache_key(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    t1 = SQLLimit(limit=100)
-    t2 = SQLLimit(limit=100)
-    assert source._get_key('test', sql_transforms=[t1]) == source._get_key('test', sql_transforms=[t2])
 
-def test_source_table_and_multi_query_sql_transform_cache_key(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    t1 = SQLLimit(limit=100)
-    t2 = SQLLimit(limit=100)
-    assert (
-        source._get_key('test', A=1, B=(3, 15.9), C=[1, 'A', 'def'], sql_transforms=[t1]) ==
-        source._get_key('test', A=1, B=(3, 15.9), C=[1, 'A', 'def'], sql_transforms=[t2])
-    )
-
-def test_file_source_get_query(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = source.get('test', A=(1, 2))
-    expected = pd._testing.makeMixedDataFrame().iloc[1:3]
-    pd.testing.assert_frame_equal(df, expected)
-
-def test_file_source_variable(make_variable_filesource):
-    root = os.path.dirname(__file__)
-    source = make_variable_filesource(root)
-    df = source.get('test')
-    expected = pd._testing.makeMixedDataFrame()
-    pd.testing.assert_frame_equal(df, expected)
-    state.variables.tables = {'test': 'test2.csv'}
-    df = source.get('test')
-    expected = pd._testing.makeMixedDataFrame().iloc[::-1].reset_index(drop=True)
-    pd.testing.assert_frame_equal(df, expected)
-
-def test_file_source_get_query_cache(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    source.get('test', A=(1, 2))
-    cache_key = source._get_key('test', A=(1, 2))
+@pytest.mark.parametrize(
+    "column_value_type", [
+        ('A', 1, 'single_value'),
+        ('A', (1, 3), 'range'),
+        ('A', (1, 2), 'range'),
+        ('A', [(0, 1), (3, 4)], 'range_list'),
+        ('C', 'foo2', 'single_value'),
+        ('C', ['foo1', 'foo3'], 'list'),
+        ('D', dt.datetime(2009, 1, 2), 'single_value'),
+        ('D', (dt.datetime(2009, 1, 2), dt.datetime(2009, 1, 5)), 'range'),
+        ('D', [dt.datetime(2009, 1, 2), dt.datetime(2009, 1, 5)], 'list'),
+        ('D', dt.datetime(2009, 1, 2), 'date'),
+        ('D', (dt.date(2009, 1, 2), dt.date(2009, 1, 5)), 'date_range'),
+    ]
+)
+@pytest.mark.parametrize("dask", [True, False])
+def test_file_source_get_query_cache(source, column_value_type, dask, expected_df):
+    column, value, _ = column_value_type
+    kwargs = {column: value}
+    source.get('test', __dask=dask, **kwargs)
+    cache_key = source._get_key('test', **kwargs)
     assert cache_key in source._cache
-    pd.testing.assert_frame_equal(
-        source._cache[cache_key],
-        pd._testing.makeMixedDataFrame().iloc[1:3]
-    )
-    cache_key = source._get_key('test', A=(1, 2))
+    cached_df = source._cache[cache_key]
+    if dask:
+        cached_df = cached_df.compute()
+    pd.testing.assert_frame_equal(cached_df, expected_df)
+    cache_key = source._get_key('test', **kwargs)
     assert cache_key in source._cache
+
 
 def test_file_source_get_query_cache_to_file(make_filesource, cachedir):
     root = os.path.dirname(__file__)
@@ -97,92 +139,11 @@ def test_file_source_get_query_cache_to_file(make_filesource, cachedir):
         pd._testing.makeMixedDataFrame().iloc[1:3]
     )
 
-def test_file_source_get_query_dask(make_filesource):
+
+def test_file_source_variable(make_variable_filesource):
     root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = source.get('test', A=(1, 2), __dask=True)
-    expected = pd._testing.makeMixedDataFrame().iloc[1:3]
+    source = make_variable_filesource(root)
+    state.variables.tables = {'test': 'test2.csv'}
+    df = source.get('test')
+    expected = pd._testing.makeMixedDataFrame().iloc[::-1].reset_index(drop=True)
     pd.testing.assert_frame_equal(df, expected)
-
-def test_file_source_get_query_dask_cache(make_filesource):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    source.get('test', A=(1, 2), __dask=True)
-    cache_key = source._get_key('test', A=(1, 2))
-    assert cache_key in source._cache
-    pd.testing.assert_frame_equal(
-        source._cache[cache_key].compute(),
-        pd._testing.makeMixedDataFrame().iloc[1:3]
-    )
-
-def test_file_source_filter_int(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', A=1)
-    expected = df[df.A==1]
-    pd.testing.assert_frame_equal(filtered, expected)
-
-def test_file_source_filter_str(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', C='foo2')
-    expected = df[df.C=='foo2']
-    pd.testing.assert_frame_equal(filtered, expected)
-
-def test_file_source_filter_int_range(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', A=(1, 3))
-    expected = df[(df.A>=1) & (df.A<=3)]
-    pd.testing.assert_frame_equal(filtered, expected)
-
-def test_file_source_filter_date(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', D=dt.date(2009, 1, 2))
-    expected = df.iloc[1:2]
-    pd.testing.assert_frame_equal(filtered, expected)
-
-def test_file_source_filter_datetime(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', D=dt.datetime(2009, 1, 2))
-    expected = df.iloc[1:2]
-    pd.testing.assert_frame_equal(filtered, expected)
-
-def test_file_source_filter_datetime_range(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', D=(dt.datetime(2009, 1, 2), dt.datetime(2009, 1, 5)))
-    expected = df.iloc[1:3]
-    pd.testing.assert_frame_equal(filtered, expected)
-
-def test_file_source_filter_date_range(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', D=(dt.date(2009, 1, 2), dt.date(2009, 1, 5)))
-    expected = df.iloc[1:3]
-    pd.testing.assert_frame_equal(filtered, expected)
-
-def test_file_source_filter_int_range_list(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', A=[(0, 1), (3, 4)])
-    expected = df[((df.A>=0) & (df.A<=1)) | ((df.A>=3) & (df.A<=4))]
-    pd.testing.assert_frame_equal(filtered, expected)
-
-def test_file_source_filter_list(make_filesource, cachedir):
-    root = os.path.dirname(__file__)
-    source = make_filesource(root)
-    df = pd._testing.makeMixedDataFrame()
-    filtered = source.get('test', C=['foo1', 'foo3'])
-    expected = df[df.C.isin(['foo1', 'foo3'])]
-    pd.testing.assert_frame_equal(filtered, expected)
