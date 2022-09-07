058cabb39052fd25c8ccd7b3b1d3f18ddae52f23
Simon Høxbro <simon.hansen@me.com>
2022-09-07 10:00:35 +0200

    Added validation test for dashboard.Dashboard

---
+++ lumen/tests/validation/test_dashboard.py
@@ -0,0 +1,54 @@
+import pytest
+
+from lumen.dashboard import Dashboard
+from lumen.validation import ValidationError
+
+
+@pytest.mark.parametrize(
+    "sources,targets,msg",
+    (
+        (
+            {'penguins': {'type': 'file', 'tables': {'penguins': 'url.com'}}},
+            [{'title': 'Table', 'source': 'penguins', 'views': []}],
+            None,
+        ),
+        (
+            {'penguin': {'type': 'file', 'tables': {'penguins': 'url.com'}}},
+            [{'title': 'Table', 'source': 'penguins', 'views': []}],
+            "Target specified non-existent source 'penguins'",
+        ),
+        (
+            {'penguins': {'type': 'file', 'tables': {'penguins': 'url.com'}}},
+            [{'title': 'Table', 'source': 'penguin', 'views': []}],
+            "Target specified non-existent source 'penguin'",
+        ),
+        (
+            {'penguins': {'type': 'file', 'tables': {'penguins': 'url.com'}}},
+            [{'title': 'Table', 'source': 'penguins', 'view': []}],
+            "Target component specification contained unknown key 'view'",
+        ),
+        (
+            {'penguins': {'tables': {'penguins': 'url.com'}}},
+            [{'title': 'Table', 'source': 'penguins', 'views': []}],
+            'Source component specification did not declare a type',
+        ),
+        (
+            {'penguins': {'type': 'file', 'tables': {'penguins': 'url.com'}}},
+            [{'source': 'penguins', 'views': []}],
+            "The Target component requires 'title' parameter to be defined",
+        ),
+    ),
+    ids=[
+        "correct", "missing_target1", "missing_target2", "missing_view",
+        "missing_type", "missing_title"
+    ]
+)
+def test_dashboard_dashboard(sources, targets, msg):
+    spec = {"sources": sources, "targets": targets}
+
+    if msg is None:
+        Dashboard.validate(spec)
+
+    else:
+        with pytest.raises(ValidationError, match=msg):
+            Dashboard.validate(spec)
