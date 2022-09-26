118fdaab9bbf6bff927e4624ad1e7a763a333b59
Simon Høxbro Hansen <simon.hansen@me.com>
2022-09-26 14:10:27 +0200

    Misc. fixes to documentation (#324)

--- docs/conf.py
+++ docs/conf.py
@@ -83,6 +83,9 @@ html_theme_options = {
     "pygment_dark_style": "material"
 }
 
+html_context = {
+    "default_mode": "light",
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
