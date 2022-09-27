381cfc7e5a9976c53a3dfdd9353ff7cd9a3c7abe
Philipp Rudiger <prudiger@anaconda.com>
2022-09-27 16:03:56 +0200

    Allow for adding widgets as filters programmatically (#332)

--- lumen/tests/validation/test_filters.py
+++ lumen/tests/validation/test_filters.py
@@ -23,8 +23,16 @@ from lumen.validation import ValidationError
             {"type": "widget", "fields": "island"},
             "The WidgetFilter component requires 'field' parameter to be defined",
         ),
+        (
+            {'type': 'widget', 'widget': 'panel.widgets.IntSlider', 'field': 'year'},
+            None
+        ),
+        (
+            {'type': 'widget', 'widget': 'IntSlider', 'field': 'year'},
+            "Filter could not resolve widget module reference 'IntSlider'."
+        ),
     ),
-    ids=["correct1", "missing_type", "missing_field_constant", "missing_field_widget"],
+    ids=["correct1", "missing_type", "missing_field_constant", "missing_field_widget", "valid_widget_ref", "wrong_widget_ref"],
 )
 def test_filter_Filter(spec, msg):
     if msg is None:
