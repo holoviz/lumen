33b18b2019a82fe3fb1ed95bfffcd532c419ee00
Philipp Rudiger <prudiger@anaconda.com>
2022-12-06 16:54:40 +0100

    Fix path reference doc path generation

--- doc/generate_reference.py
+++ doc/generate_reference.py
@@ -225,7 +225,7 @@ def generate_component_index(base):
 def generate_multi_component_pages(base):
     index = generate_component_index(base)
     component_path = REFERENCE_PATH /  base.__name__.lower()
-    component_path.mkdir(exist_ok=True)
+    component_path.mkdir(exist_ok=True, parents=True)
     with open(component_path / 'index.md', 'w') as f:
         f.write(index)
     for component in concrete_descendents(base).values():
@@ -266,7 +266,7 @@ def write_index(bases):
         f.write(page)
 
 if __name__ == '__main__':
-    REFERENCE_PATH.mkdir(exist_ok=True)
+    REFERENCE_PATH.mkdir(exist_ok=True, parents=True)
     for component in bases:
         if issubclass(component, MultiTypeComponent):
             generate_multi_component_pages(component)
