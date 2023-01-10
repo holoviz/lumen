ae6a272ad2d35e7274cadea1eb97e5f185d44e3e
Philipp Rudiger <prudiger@anaconda.com>
2023-01-10 13:38:53 +0100

    Add Sum, Count and Eval transforms (#425)
    * Add Sum, Count and Eval transforms
    
    * Add tests

--- lumen/tests/transforms/test_base.py
+++ lumen/tests/transforms/test_base.py
@@ -1,9 +1,12 @@
 import os
 import pathlib
 
+import pandas as pd
 import param  # type: ignore
 
-from lumen.transforms.base import Transform
+from lumen.transforms.base import (
+    Count, Eval, Sum, Transform,
+)
 
 
 class CustomTransform(Transform):
@@ -43,3 +46,30 @@ def test_transform_control_options_by_reference(make_filesource):
         }]
     })
     assert transform.param.value.objects == ['foo1', 'foo2', 'foo3', 'foo4', 'foo5']
+
+
+def test_count_transform():
+    df = pd._testing.makeMixedDataFrame()
+
+    count_df = Count.apply_to(df)
+
+    pd.testing.assert_frame_equal(count_df, df.count().to_frame().T)
+
+
+def test_sum_transform():
+    df = pd._testing.makeMixedDataFrame()[['A']]
+
+    sum_df = Sum.apply_to(df)
+
+    pd.testing.assert_frame_equal(sum_df, df.sum().to_frame().T)
+
+
+def test_eval_transform():
+    df = pd._testing.makeMixedDataFrame()[['A']]
+
+    eval_df = Eval.apply_to(df, expr='B = table.A * 2')
+
+    df2 = df.copy()
+    df2['B'] = df.A * 2
+
+    pd.testing.assert_frame_equal(eval_df, df2)
