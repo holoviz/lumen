ea36c94cf047b5c343c91aeb4e00383c2f392750
Philipp Rudiger <prudiger@anaconda.com>
2023-02-10 17:20:54 +0100

    Fix DuckDB distinct expression

--- lumen/sources/duckdb.py
+++ lumen/sources/duckdb.py
@@ -101,7 +101,7 @@ class DuckDBSource(Source):
                 elif 'inclusiveMinimum' in col_schema:
                     min_maxes.append(name)
             for col in enums:
-                distinct_expr = sql_expr.apply(SQLDistinct(columns=[col]))
+                distinct_expr = SQLDistinct(columns=[col]).apply(sql_expr)
                 distinct_expr = ' '.join(distinct_expr.splitlines())
                 distinct = self._connection.execute(distinct_expr).fetch_df()
                 schema[col]['enum'] = distinct[col].tolist()
