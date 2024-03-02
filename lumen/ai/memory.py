af3e5c66ab2a23179bffa74bd53bab752813800e
Philipp Rudiger <prudiger@anaconda.com>
2024-03-02 11:54:24 +0100

    Improve Memory rendering

--- lumen/ai/memory.py
+++ lumen/ai/memory.py
@@ -42,26 +42,39 @@ class _Memory(Viewer):
     def __setitem__(self, key, value):
         self._curcontext[key] = value
         if state.curdoc in self._views:
-            self._views[state.curdoc][:] = [self._create_view()]
+            self._update_view(key, value)
 
     def get(self, key, default=None):
         return self._curcontext.get(key, default)
 
-    def _render_item(self, item):
+    def _render_item(self, key, item):
         if isinstance(item, Component):
             item = item.to_spec()
         if isinstance(item, str):
             item = f'```yaml\n{item}\n```'
-        return pn.panel(item, sizing_mode='stretch_width')
+        return pn.panel(item, name=key, sizing_mode='stretch_width', styles={'overflow': 'scroll'})
 
-    def _create_view(self):
+    def _render_memories(self):
         return pn.Accordion(*(
-            (name, self._render_item(item))
+            self._render_item(name, item)
             for name, item in self._curcontext.items()
         ), sizing_mode='stretch_width', active=list(range(len(self._curcontext))))
 
+    def _update_view(self, key, value):
+        view = self._views[state.curdoc]
+        accordion = view[0]
+        new_item = self._render_item(key, value)
+        i = 0
+        for i, item in enumerate(accordion):
+            if item.name == key:
+                accordion[i] = new_item
+                break
+        else:
+            accordion.append(new_item)
+            accordion.active = accordion.active + [i+1]
+
     def __panel__(self):
-        self._views[state.curdoc] = view = pn.Column(self._create_view())
+        self._views[state.curdoc] = view = pn.Column(self._render_memories())
         return view
 
 
