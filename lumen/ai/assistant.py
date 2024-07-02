7bf7b1eb206ed8a2de14ce7c1ec2170f0b0c867f
Andrew Huang <ahuang@anaconda.com>
2024-07-02 14:58:39 -0700

    increase load buffer

--- lumen/ai/assistant.py
+++ lumen/ai/assistant.py
@@ -122,7 +122,7 @@ class Assistant(Viewer):
 
         if interface is None:
             interface = ChatInterface(
-                callback=self._chat_invoke, load_buffer=5,
+                callback=self._chat_invoke, load_buffer=100,
             )
         else:
             interface.callback = self._chat_invoke
