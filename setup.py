55940675f7b79b5eea6c9b0529cc6753e5e5b2c4
Andrew <15331990+ahuang11@users.noreply.github.com>
2024-07-03 12:29:50 -0700

    add ai extra setup (#588)

--- setup.py
+++ setup.py
@@ -28,6 +28,15 @@ dependencies = [
 ]
 
 extras_require = {
+    'ai': [
+        'nbformat',
+        'duckdb',
+        'pyarrow',
+        'openai',
+        'instructor',
+        'pydantic',
+        'datashader',   # for big datasets
+    ],
     'sql': [
         'duckdb',
         'intake-sql',
@@ -54,7 +63,8 @@ extras_require = {
         'nbsite >=0.8.2',
     ]
 }
-
+extras_require["ai_local"] = extras_require["ai"] + ["huggingface_hub"]
+extras_require["ai_llama"] = extras_require["ai_local"] + ["llama-cpp-python"]
 extras_require['all'] = sorted(set(sum(extras_require.values(), [])))
 
 ########## metadata for setuptools ##########
