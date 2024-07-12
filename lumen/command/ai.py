0ae48bce76e69900508ad529ed7c7417e62219f0
Demetris Roumis <roumis.d@gmail.com>
2024-07-12 09:55:03 -0700

    use a custom argparser to provide relevant --help output

--- lumen/command/ai.py
+++ lumen/command/ai.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import argparse
 import ast
 import os
 import sys
@@ -8,10 +9,10 @@ import bokeh.command.util  # type: ignore
 
 from bokeh.application.handlers.code import CodeHandler  # type: ignore
 from bokeh.command.util import (  # type: ignore
-    build_single_handler_application as _build_application,
+    build_single_handler_application as _build_application, die,
 )
 from bokeh.document import Document
-from panel.command import main as _pn_main
+from panel.command import Serve, transform_cmds
 from panel.io.server import Application
 from panel.io.state import set_curdoc
 
@@ -121,7 +122,41 @@ def main(args=None):
         sys.argv = sys.argv[:start] + sys.argv[end+1:]
         config.template_vars = ast.literal_eval(template_vars)
 
-    _pn_main()
+    parser = argparse.ArgumentParser(
+        prog="lumen-ai", epilog="See '<command> --help' to read about a specific subcommand."
+    )
+
+    parser.add_argument(
+        '-v', '--version', action='version', version='Lumen AI 1.0.0'
+    )
+
+    subs = parser.add_subparsers(help="Sub-commands")
+
+    serve_parser = subs.add_parser(Serve.name, help=Serve.help)
+    serve_command = Serve(parser=serve_parser)
+    serve_parser.set_defaults(invoke=serve_command.invoke)
+
+    if len(sys.argv) == 1:
+        all_commands = [Serve.name]
+        die(f"ERROR: Must specify subcommand, one of: {', '.join(all_commands)}")
+
+    if sys.argv[1] in ('--help', '-h'):
+        args = parser.parse_args(sys.argv[1:])
+        args.invoke(args)
+        sys.exit()
+
+    sys.argv = transform_cmds(sys.argv)
+    args = parser.parse_args(sys.argv[1:])
+
+    try:
+        ret = args.invoke(args)
+    except Exception as e:
+        die("ERROR: " + str(e))
+
+    if ret is False:
+        sys.exit(1)
+    elif ret is not True and isinstance(ret, int) and ret != 0:
+        sys.exit(ret)
 
 
 if __name__ == "__main__":
