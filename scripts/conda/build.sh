d8fb599c1b603b185360a0461a76447dd50c0aef
Simon Høxbro Hansen <simon.hansen@me.com>
2024-07-31 15:54:51 +0200

    Use old pkg_format for now

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -9,7 +9,8 @@ python -m build -w .
 VERSION=$(python -c "import $PACKAGE; print($PACKAGE._version.__version__)")
 export VERSION
 
-conda config --env --set conda_build.pkg_format 2
+# conda config --env --set conda_build.pkg_format 2
 conda build scripts/conda/recipe --no-anaconda-upload --no-verify
 
-mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.conda" dist
+# mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.conda" dist
+mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.tar.bz2" dist
