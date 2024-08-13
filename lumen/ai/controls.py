ba38a771bf2d8598439d13ce697aecdadfef590b
Andrew Huang <ahuang@anaconda.com>
2024-08-13 14:17:39 -0700

    use dict

--- lumen/ai/controls.py
+++ lumen/ai/controls.py
@@ -21,6 +21,8 @@ def add_source_controls(replace_controls: bool = True):
                     df = pd.read_excel(io.BytesIO(upload.value))
                 # TODO: add url support
                 duckdb_source._connection.from_df(df).to_table(name.value)
+                if duckdb_source.tables is None:
+                    duckdb_source.tables = {}
                 duckdb_source.tables[name.value] = f"SELECT * FROM {name.value}"
                 memory["current_source"] = duckdb_source
                 memory["current_table"] = name.value
