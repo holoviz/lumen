2afa6a807548f77564e35b90b406c84d6102da82
Andrew Huang <ahuang@anaconda.com>
2024-08-15 17:14:41 -0700

    make slightly more robust

--- lumen/ai/agents.py
+++ lumen/ai/agents.py
@@ -513,7 +513,7 @@ class SQLAgent(LumenBaseAgent):
         if len(sources) > 1:
             mirrors = {}
             for a_source, a_table in sources.values():
-                if not any(ext in a_table for ext in [".csv", ".parquet", ".parq", ".json", ".xlsx"]):
+                if not any(a_table.endswith(ext) for ext in [".csv", ".parquet", ".parq", ".json", ".xlsx"]):
                     renamed_table = a_table.replace(".", "_")
                     sql_query = sql_query.replace(a_table, renamed_table)
                 else:
