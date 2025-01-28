a5be9f9590036922d7d0b8c10a6a6a95b01bf9b8
Philipp Rudiger <prudiger@anaconda.com>
2025-01-28 16:25:03 +0100

    Small tweaks (#984)

--- lumen/ai/agents.py
+++ lumen/ai/agents.py
@@ -335,7 +335,7 @@ class TableListAgent(Agent):
             widths={'Table': '90%'},
             disabled=True,
             page_size=10,
-            header_filters=True
+            header_filters=len(tables) > 10
         )
         table_list.on_click(self._use_table)
         self.interface.stream(table_list, user="Lumen")
