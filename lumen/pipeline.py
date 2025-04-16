b8ac16e77c4c527a55245eb1c6a78acae576f1aa
Andrew Huang <ahuang@anaconda.com>
2025-04-16 16:21:57 -0700

    Remove sql_metaset requirement

--- lumen/pipeline.py
+++ lumen/pipeline.py
@@ -468,8 +468,9 @@ class Pipeline(Viewer, Component):
                 resolved_table = state.resolve_reference(table) if is_ref(table) else table
                 resolved_source._get_source(resolved_table)
             else:
+                print(resolved_source, type(resolved_source))
                 raise ValidationError(
-                    "The Pipeline specification references a table that is not "
+                    f"The Pipeline specification references a table {table} that is not "
                     "available on the specified source.", spec
                 )
 
