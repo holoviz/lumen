ec71dd8608d91f53004d36c5ef6adcfc99bcc714
Philipp Rudiger <prudiger@anaconda.com>
2025-07-16 18:45:26 +0200

    Change Plan.abort_on_error=True

--- lumen/ai/coordinator.py
+++ lumen/ai/coordinator.py
@@ -45,7 +45,7 @@ class Plan(Section):
     A Plan is a Task that is a collection of other Tasks.
     """
 
-    abort_on_error = param.Boolean(default=False, doc="""
+    abort_on_error = param.Boolean(default=True, doc="""
         If True, the report will abort if an error occurs.""")
 
     agents = param.List(item_type=Actor, default=[])
