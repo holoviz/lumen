0f3aac12f03ceb360896f6da19c96bcf6b99a389
Philipp Rudiger <prudiger@anaconda.com>
2025-07-30 18:47:07 +0200

    Copy curcontext

--- lumen/config.py
+++ lumen/config.py
@@ -94,7 +94,7 @@ class SessionCache:
         new._global_context = self._global_context.copy()
         new._session_contexts = self._session_contexts.copy()
         if state.curdoc and state.curdoc in self._session_contexts:
-            new._session_contexts[state.curdoc] = self._curcontext
+            new._session_contexts[state.curdoc] = self._curcontext.copy()
         return new
 
 
