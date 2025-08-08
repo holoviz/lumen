ab09af1e8ff40509a4fdf6ce1a5013ae940284c5
Philipp Rudiger <prudiger@anaconda.com>
2025-08-08 10:52:05 +0200

    Apply suggestions from code review

--- lumen/tests/sources/test_duckdb.py
+++ lumen/tests/sources/test_duckdb.py
@@ -537,8 +537,6 @@ def test_absolute_vs_relative_paths(sample_csv_files):
     finally:
         os.chdir(original_cwd)
 
-
-
 def test_mirrors_not_in_tables_from_spec():
     """Test that mirrors are not included in the tables from_spec."""
     spec = DuckDBSource(tables={"obs": "SELECT * FROM obs"}).to_spec()
