23f5f122a8017ab03886afa57d10b34f0bede645
Andrew Huang <ahuang@anaconda.com>
2025-08-25 17:33:45 -0700

    raise errors

--- lumen/ai/agents.py
+++ lumen/ai/agents.py
@@ -94,12 +94,9 @@ class Agent(Viewer, ToolUser, ContextProvider):
                 return
 
             import traceback
-
             traceback.print_exception(exception)
             self.interface.send(f"Error cannot be resolved:\n\n{exception}", user="System", respond=False)
 
-        if "interface" not in params:
-            params["interface"] = ChatInterface(callback=self._interface_callback)
         super().__init__(**params)
         if not self.debug:
             pn.config.exception_handler = _exception_handler
