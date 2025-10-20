c7fc2de9386de35184df002a1fa95c4b940477af
Andrew <15331990+ahuang11@users.noreply.github.com>
2025-10-20 08:34:26 -0700

    Apply suggestion from @ahuang11

--- lumen/ai/schemas.py
+++ lumen/ai/schemas.py
@@ -144,7 +144,7 @@ class SQLMetaset:
 
             base_sql = truncate_string(vector_metadata.base_sql, max_length=200) if truncate else vector_metadata.base_sql
 
-            table_data = {'access': base_sql}
+            table_data = {'read_with': base_sql}
 
             if vector_metadata.description:
                 desc = truncate_string(vector_metadata.description, max_length=100) if truncate else vector_metadata.description
