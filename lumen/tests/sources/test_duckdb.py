2d864c5c79f5da86d2a00fc8f016032e6f0f37d9
Philipp Rudiger <prudiger@anaconda.com>
2025-11-20 21:51:17 +0100

    Small fix

--- lumen/tests/sources/test_duckdb.py
+++ lumen/tests/sources/test_duckdb.py
@@ -180,7 +180,7 @@ def test_duckdb_clear_cache(duckdb_source):
 def test_duckdb_source_ephemeral_roundtrips(duckdb_memory_source, mixed_df):
     source = DuckDBSource.from_spec(duckdb_memory_source.to_spec())
     df = source.get('mixed')
-    assert df.equal(mixed_df)
+    assert df.equals(mixed_df)
 
 
 def test_duckdb_source_mirrors_source(duckdb_source):
